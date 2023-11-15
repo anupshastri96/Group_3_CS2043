ee390d0e47f0e58bcd4d6d1308361f53deac272e
Crab <128949281+TheCrabJesus@users.noreply.github.com>
2023-11-15 14:13:29 -0400

    Alex's push

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.unb.budgetmaster.budgetmaster2043 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.unb.budgetmaster.budgetmaster2043 to javafx.fxml;
-    exports com.unb.budgetmaster.budgetmaster2043;
-}
