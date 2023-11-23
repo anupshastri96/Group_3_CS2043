21d857b1c4d305a7331d833c712b5754964cb694
Crab <128949281+TheCrabJesus@users.noreply.github.com>
2023-11-23 15:56:05 -0400

    Added test case, however implementation isn't actually added to make it work

--- Desktop/JavaProjects/BudgetMaster/src/main/java/com/unb/budgetmaster/budgetmaster/domain/model/Transaction.java
+++ Desktop/JavaProjects/BudgetMaster/src/main/java/com/unb/budgetmaster/budgetmaster/domain/model/Transaction.java
@@ -3,11 +3,46 @@ package com.unb.budgetmaster.budgetmaster.domain.model;
 import com.unb.budgetmaster.budgetmaster.presentation.Category;
 
 public class Transaction {
-    String date ;
+    String date;
     int id;
-    double amount ;
+    double amount;
     String payee; 
     String type; 
     //Category category;
     String category;
+
+    public Transaction(String date, int id, double amount, String payee, String type, String category) {
+        this.date = date;
+        this.id = id;
+        this.amount = amount;
+        this.payee = payee;
+        this.type = type;
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
+
+
