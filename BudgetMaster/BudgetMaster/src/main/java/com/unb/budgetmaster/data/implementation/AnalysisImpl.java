0033198b568c7dd6a147532e4aeaf2fb598ed5b0
Minh Nguyen <minhnguyen4424@gmail.com>
2023-11-27 16:05:36 -0400

    Finished methods for AnalysisImpl

--- BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/data/implementation/AnalysisImpl.java
+++ BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/data/implementation/AnalysisImpl.java
@@ -1,8 +1,11 @@
 package com.unb.budgetmaster.data.implementation;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.util.Calendar;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import com.unb.budgetmaster.data.implementation.DatabaseImpl;
@@ -10,6 +13,7 @@ import com.unb.budgetmaster.domain.abs.AnalysisABS;
 import com.unb.budgetmaster.data.implementation.TransactionImpl;
 
 public class AnalysisImpl implements AnalysisABS{
+    public static LocalDate date = LocalDate.now();
     DatabaseImpl data = new DatabaseImpl();
     TransactionImpl transaction = new TransactionImpl();
     Connection connection = data.connectDatabase();
@@ -18,11 +22,11 @@ public class AnalysisImpl implements AnalysisABS{
     public double getTotalSpent(String username) {
         double total = 0;
         try{
-            String totalSpentQuery = "select spendings_overall_total from transaction_data where user_name = " + username + ";";
+            String totalSpentQuery = "select transaction_amount from transaction_data where transaction_type = 'Spendings' and user_name = " + username  + ";";
             PreparedStatement statement = connection.prepareStatement(totalSpentQuery);
-            ResultSet results = statement.executeQuery(); 
+            ResultSet results = statement.executeQuery();
             if(results.next()){
-                total = results.getDouble("spendings_overall_total");
+                total += results.getDouble("transaction_amount");
             }
         }
         catch(SQLException e){
@@ -33,31 +37,79 @@ public class AnalysisImpl implements AnalysisABS{
 
     @Override
     public double getTotalSaved(String username) {
-        return 0;
+        double total = 0;
+        try{
+            String totalSavedQuery = "select transaction_amount from transaction_data where transaction_type = 'Savings' and user_name = " + username + ";";
+            PreparedStatement statement = connection.prepareStatement(totalSavedQuery);
+            ResultSet results = statement.executeQuery();
+            if(results.next()){
+                total += results.getDouble("transaction_amount");
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return total;
     }
 
     @Override
-    public double getTotalSpent(String date1, String date2, String username) {
-        return 0;
+    public double getTotalSpent(LocalDate date1, LocalDate date2, String username) {
+        double total = 0;
+        Date d1 = Date.valueOf(date1);
+        Date d2 = Date.valueOf(date2);
+        try{
+            String queryTotalSpent = "select transaction_amount from transaction_data where transaction_date >= '" + d1 + "' and transaction_date <= '" + d2 + "' and transaction_type = 'Spendings' and user_name = " + username + ";";
+            PreparedStatement statement = connection.prepareStatement(queryTotalSpent);
+            ResultSet results = statement.executeQuery();
+            if(results.next()){
+                total += results.getDouble("transaction_amount");
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return total;
     }
 
     @Override
-    public double getTotalSaved(String date1, String date2, String username) {
-        return 0;
+    public double getTotalSaved(LocalDate date1, LocalDate date2, String username) {
+        double total = 0;
+        Date d1 = Date.valueOf(date1);
+        Date d2 = Date.valueOf(date2);
+        try{
+            String queryTotalSaved = "select transaction_amount from transaction_data where transaction_date >= '" + d1 + "' and transaction_date <= '" + d2 + "' and transaction_type = 'Savings' and user_name = " + username + ";";
+            PreparedStatement statement = connection.prepareStatement(queryTotalSaved);
+            ResultSet results = statement.executeQuery();
+            if(results.next()){
+                total += results.getDouble("transaction_amount");
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return total;
     }
 
     @Override
     public double getUsualSpent(String username) {
-        return 0;
+        int lengthOfMonth = date.lengthOfMonth();
+        double totalAmountSpent = this.getTotalSpent(username);
+        double total = totalAmountSpent/lengthOfMonth;
+
+        return total;
     }
     @Override
-    public double getUsualSpent(String date1, String date2, String username) {
-     return 0;
+    public double getUsualSpent(LocalDate date1, LocalDate date2, String username) {
+        int lengthOfMonth = date.lengthOfMonth();
+        double totalAmountSpent = this.getTotalSpent(date1, date2, username);
+        double total = totalAmountSpent/lengthOfMonth;
+        return total;
     }
 
     @Override
     public double getBalance(String username) {
-        return 0;
+        double total = this.getTotalSpent(username) - this.getTotalSaved(username);
+        return total;
     }
     
 }
--- BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/domain/abs/AnalysisABS.java
+++ BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/domain/abs/AnalysisABS.java
@@ -1,11 +1,12 @@
 package com.unb.budgetmaster.domain.abs;
+import java.time.LocalDate;
 
 public interface AnalysisABS {
     double getTotalSpent(String username);
     double getTotalSaved(String username);
-    double getTotalSpent(String date1, String date2, String username);
-    double getTotalSaved(String date1, String date2, String username);
+    double getTotalSpent(LocalDate date1, LocalDate date2, String username);
+    double getTotalSaved(LocalDate date1, LocalDate date2, String username);
     double getUsualSpent(String username);
-    double getUsualSpent(String date1, String date2, String username);
+    double getUsualSpent(LocalDate date1, LocalDate date2, String username);
     double getBalance(String username);
 }
