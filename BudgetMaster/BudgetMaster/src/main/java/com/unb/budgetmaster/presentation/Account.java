9a56d05b3e352199924c6ba3cca7b596d242b7df
Alex Boudreau <128949281+TheCrabJesus@users.noreply.github.com>
2023-11-28 01:36:51 -0400

    Added Account class

--- BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/presentation/Account.java
+++ BudgetMaster/BudgetMaster/src/main/java/com/unb/budgetmaster/presentation/Account.java
@@ -2,25 +2,36 @@ package com.unb.budgetmaster.presentation;
 
 import com.unb.budgetmaster.data.implementation.AnalysisImpl;
 import com.unb.budgetmaster.data.implementation.Database;
-import com.unb.budgetmaster.data.implementation.TransactionImpl;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class Account {
     // Interface Implementation
     private AnalysisImpl analysisImpl;
+
+    // UI Implementation
+    private LoginUI loginUI;
 
     // Buttons
     Button editBudgetButton;
     Button changePasswordButton;
     Button logOutButton;
 
-    public void getContent(Label contentLabel, VBox contentContainer) {
-        //Instantiate implementations
+    Label currentBudget_lb;
+
+    public void getContent(Label contentLabel, VBox contentContainer, BorderPane root) {
+        // Instantiate implementations
         analysisImpl = new AnalysisImpl();
+
+        // UI instances
+        loginUI = new LoginUI();
 
         // Modify contentLabel
         contentLabel.setText("Account Details");
@@ -32,7 +43,7 @@ public class Account {
         Label balance_lb = new Label("Balance: " + analysisImpl.getBalance());
 
         // Create Label for Current Budget
-        Label currentBudget_lb = new Label("Current Budget: " + analysisImpl.getBudgetTotal());
+        currentBudget_lb = new Label("Current Budget: " + analysisImpl.getBudgetTotal());
 
         // Setup buttons
         editBudgetButton = new Button("Edit");
@@ -40,7 +51,7 @@ public class Account {
         changePasswordButton = new Button("Change Password");
         changePasswordButton.setOnAction(event -> changePassword());
         logOutButton = new Button("Log Out");
-        logOutButton.setOnAction(event -> logOut());
+        logOutButton.setOnAction(event -> logOut(root));
 
         // Create HBox for Budget label and Edit Budget button
         HBox budgetBox = new HBox(20);
@@ -57,16 +68,90 @@ public class Account {
         contentContainer.setSpacing(20);
     }
 
-    public void editBudget() {
-        
+    private void editBudget() {
+        Stage editStage = new Stage();
+        editStage.setTitle("Edit Budget");
+        editStage.initModality(Modality.APPLICATION_MODAL);
+
+        Label newBudget_lb = new Label("Enter a new Budget Amount");
+        TextField budget_tf = new TextField();
+
+        Button backButton = new Button("Back");
+        backButton.setOnAction(event -> back(editStage));
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(event -> editSubmit(budget_tf.getText(), budget_tf, editStage));
+
+        HBox buttonBox = new HBox(10);
+        buttonBox.getChildren().addAll(backButton, submitButton);
+
+        VBox editBox = new VBox(10);
+        editBox.getChildren().addAll(newBudget_lb, budget_tf, buttonBox);
+
+    }
+    
+    private void back(Stage stage) {
+        stage.close();
+        return;
     }
 
-    public void changePassword() {
+    private void editSubmit(String newBudget, TextField budget_tf, Stage stage) {
+        if(newBudget.equals(null)) {
+            budget_tf.clear();
+            budget_tf.setPromptText("Please enter a value");
+            return;
+        }
 
+        double budgetDouble;
+
+        try {
+            budgetDouble = Double.parseDouble(newBudget);
+        } catch (NumberFormatException e) {
+            // Handle invalid input
+            System.out.println("Invalid date format");
+            return;
+        }
+
+        Database.user.setBudgetTotal(budgetDouble);
+        currentBudget_lb.setText("Current Budget: " + analysisImpl.getBudgetTotal());
+        stage.close();
+        return;
     }
 
-    public void logOut() {
+    private void changePassword() {
+        Stage pWStage = new Stage();
+        pWStage.setTitle("Change Password");
+        pWStage.initModality(Modality.APPLICATION_MODAL);
 
+        Label newPW_lb = new Label("Enter a new Password");
+        TextField pW_tf = new TextField();
+
+        Button backButton = new Button("Back");
+        backButton.setOnAction(event -> back(pWStage));
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(event -> passwordSubmit(pW_tf.getText(), pW_tf, pWStage));
+
+        HBox buttonBox = new HBox(10);
+        buttonBox.getChildren().addAll(backButton, submitButton);
+
+        VBox editBox = new VBox(10);
+        editBox.getChildren().addAll(newPW_lb, pW_tf, buttonBox);
+    }
+
+    private void passwordSubmit(String newPW, TextField pW_tf, Stage stage) {
+        if(newPW.equals(null)) {
+            pW_tf.clear();
+            pW_tf.setPromptText("Please enter a new password");
+            return;
+        }
+
+        Database.user.setPassword(newPW);
+        stage.close();
+        return;
+    }
+
+    private void logOut(BorderPane root) {
+        loginUI.getLoginUI(root);
+        return;
     }
 }
 // End of Account class
